6d0338e8820fa49e67f263bcae8a0c0325ed12dd
Khaqan Chaudhary <44577596+khaqannadeem@users.noreply.github.com>
2018-11-30 05:53:09 -0800

    quiz 4

---
+++ quiz-3/quiz-4/js/script.js
@@ -0,0 +1,78 @@
+var questions = [{
+    question : "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+    choices : [ "The User's machine running a Web browser",
+        "The Web server",
+        "A central machine deep within Netscape's corporate offices",
+        "None of the above"],
+    correctAnswer : 0
+},{
+    question : "Which of the following can't be done with client-side JavaScript?",
+    choices : [ "Validating a form",
+        "Sending a form's contents by email",
+        "Storing the form's contents to a database file on the server",
+        "None of the above"],
+    correctAnswer : 2
+},{
+    question : "Using _______ statement is how you test for a specific condition",
+    choices : [ "select",
+        "if",
+        "for",
+        "none of the above"],
+    correctAnswer : 1
+},
+    { question : "Prime minister of Pakistan",
+    choices : [ "Shahbaz Shareef",
+    "Imran Khan",
+    "Nawaz Shareef",
+    "none of the above"],
+    correctAnswer : 2
+},
+
+
+    { question : "Capital of Pakistan",
+        choices : [ "Lahore",
+            "Islamabad",
+            "Rawal Pindi",
+            "none of the above"],
+        correctAnswer : 2
+    }
+];
+
+var currentQuestion = 0;
+var correctAnswers = 0;
+var quizOver = false;
+displayCurrentQuestion();
+document.getElementById("quiz-message").style.display = 'none';
+function displayNext() {
+    document.getElementById("quiz-message").innerText=questions[currentQuestion++].question;
+    document.getElementById("choice-list").innerHTML = " ";
+    for (var i=0; i<4; i++)
+    {
+        document.getElementById("choice-list").innerHTML += '<li><input type = "radio">' +questions[currentQuestion].choices[i]+'</li>';
+    }
+
+
+}
+
+function displayCurrentQuestion() {
+    document.getElementById("question").innerText=questions[currentQuestion].question;
+    document.getElementById("choice-list").innerHTML = "";
+    for (var i=0; i<4; i++)
+    {
+        document.getElementById("choice-list").innerHTML += '<li><input type = "radio">' +questions[currentQuestion].choices[i]+'</li>';
+    }
+
+}
+
+function resetQuiz() {
+    currentQuestion = 0;
+    correctAnswers = 0;
+    hideScore();
+}
+function displayScore() {
+    document.getElementById("result").innerHTML = "you scored: " + correctAnswers + " out of: " + questions.length;
+    document.getElementById("result").style.display = 'block';
+}
+function hideScore() {
+    document.getElementById("result").style.display = 'none';
+}
